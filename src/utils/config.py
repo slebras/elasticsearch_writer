b2b1f8f34480dc8e3818c37a253f36e65148cc12
Jay R Bolton <jayrbolton@gmail.com>
2019-05-06 15:05:26 -0700

    Improve config syntax

--- src/utils/config.py
+++ src/utils/config.py
@@ -5,14 +5,15 @@ def get_config():
     """
     Initialize configuration data.
     """
-    config = {
+    es_host = os.environ.get("ELASTICSEARCH_HOST", 'elasticsearch')
+    es_port = os.environ.get("ELASTICSEARCH_PORT", 9200)
+    return {
         'kafka_server': os.environ.get('KAFKA_SERVER', 'kafka'),
         'kafka_clientgroup': os.environ.get('KAFKA_CLIENTGROUP', 'search_indexer'),
         'elasticsearch_host': os.environ.get("ELASTICSEARCH_HOST", 'elasticsearch'),
         'elasticsearch_port': os.environ.get("ELASTICSEARCH_PORT", 9200),
         'elasticsearch_index_prefix': os.environ.get("ELASTICSEARCH_INDEX_PREFIX", "search2"),
         'elasticsearch_data_type': os.environ.get("ELASTICSEARCH_DATA_TYPE", "data"),
+        'elasticsearch_url': f"//{es_host}:{es_port}",
         'elasticsearch_save_topic': os.environ.get('KAFKA_ES_UPDATE_TOPIC', 'elasticsearch_updates')
     }
-    config['elasticsearch_url'] = f"http://{config['elasticsearch_host']}:{config['elasticsearch_port']}"
-    return config
